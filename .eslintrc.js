cd5440c219de7dfc472d86114b5fbd2ce7db3bd9
Vinicius Ribeiro <vinicius.fernandes@escale.com.br>
2018-10-31 09:59:47 -0300

    Configure jsPrettier and Airbnb Linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,28 @@
 module.exports = {
-    "extends": "airbnb"
-};
+  'env': {
+    'browser': true,
+    'jest': true,
+    'es6': true,
+    'node': true,
+  },
+  'extends': [
+    'airbnb',
+    'prettier',
+  ],
+  'plugins': [
+    'prettier',
+  ],
+  'rules': {
+    'prettier/prettier': [
+      'error', {
+        'singleQuote': true,
+        'trailingComma': 'es5',
+      },
+    ],
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+        'jsx': true,
+    }
+  }
+}
